a463bf055159f89c4fc3166da878625f2fb681fd
eediallo <elhhabdouldiallo@gmail.com>
2025-05-09 10:19:40 +0100

    feat: implement uploadProductImage function to handle file uploads and return image path

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,6 +1,20 @@
+import path from "path";
+
 import { Product } from "../models/Product.js";
 import { StatusCodes } from "http-status-codes";
+import { STATUS_CODES } from "http";
 
-export const uploadProductImage = (req, res) => {
-  res.send("uploads product");
+export const uploadProductImage = async (req, res) => {
+  const productImage = req.files.image;
+  console.log(productImage);
+
+  const imagePath = new URL(
+    `../public/uploads/${productImage.name}`,
+    import.meta.url
+  ).pathname;
+
+  await productImage.mv(imagePath);
+  res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `uploads/${productImage.name}` } });
 };
