2ee4db2b9aed5da902b6a51f05cb13eff117f563
eediallo <elhhabdouldiallo@gmail.com>
2025-05-09 10:56:01 +0100

    fix: update uploadProductImage function to correctly handle image uploads and improve error handling

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -2,19 +2,33 @@ import path from "path";
 
 import { Product } from "../models/Product.js";
 import { StatusCodes } from "http-status-codes";
-import { STATUS_CODES } from "http";
+import { BadRequestError } from "../errors/index.js";
 
 export const uploadProductImage = async (req, res) => {
-  const productImage = req.files.image;
-  console.log(productImage);
+  const productImage = req.files;
+  console.log(req.files);
+
+  if (!productImage) {
+    throw new BadRequestError("No image uploaded.");
+  }
+
+  if (!productImage.image.mimetype.startsWith("image")) {
+    throw new BadRequestError("Please upload an image");
+  }
+
+  const maxSize = 1024 * 1024;
+
+  if (productImage.image.size > maxSize) {
+    throw new BadRequestError(`Image size must be less than ${maxSize}`);
+  }
 
   const imagePath = new URL(
-    `../public/uploads/${productImage.name}`,
+    `../public/uploads/${productImage.image.name}`,
     import.meta.url
   ).pathname;
 
-  await productImage.mv(imagePath);
+  await productImage.image.mv(imagePath);
   res
     .status(StatusCodes.OK)
-    .json({ image: { src: `uploads/${productImage.name}` } });
+    .json({ image: { src: `uploads/${productImage.image.name}` } });
 };
