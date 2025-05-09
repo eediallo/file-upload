4e3de19230ab3de939235bd12c4849cf04d09b8a
eediallo <elhhabdouldiallo@gmail.com>
2025-05-09 11:14:05 +0100

    feat: configure cloudinary for image uploads

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { connectDB } from "./db/connect.js";
 import { notFound } from "./middleware/not-found.js";
 import { errorHandlerMiddleware } from "./middleware/error-handler.js";
 import fileUpload from "express-fileupload";
+import { v2 as cloudinary } from "cloudinary";
 
 // product router
 import { productRouter } from "./routes/productRoutes.js";
@@ -14,6 +15,13 @@ const app = express();
 app.use(express.static("./public")); // make publicly available
 app.use(express.json());
 app.use(fileUpload());
+
+// config cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 app.get("/", (req, res) => {
   res.send("<h1>File Upload Starter</h1>");
