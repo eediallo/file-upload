1a97ee02de20fc15f9d522270e6fac985a9bcabf
eediallo <elhhabdouldiallo@gmail.com>
2025-05-09 11:42:28 +0100

    use cloudinary to upload images

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -2,8 +2,9 @@ import path from "path";
 
 import { StatusCodes } from "http-status-codes";
 import { BadRequestError } from "../errors/index.js";
+import { v2 as cloudinary } from "cloudinary";
 
-export const uploadProductImage = async (req, res) => {
+export const uploadProductImageLocal = async (req, res) => {
   const productImage = req.files;
   console.log(req.files);
 
@@ -30,4 +31,58 @@ export const uploadProductImage = async (req, res) => {
   res
     .status(StatusCodes.OK)
     .json({ image: { src: `uploads/${productImage.image.name}` } });
+};
+
+export const uploadProductImage = async (req, res) => {
+  const productImage = req.files;
+
+  if (!productImage) {
+    throw new BadRequestError("No image uploaded.");
+  }
+
+  if (!productImage.image.mimetype.startsWith("image")) {
+    throw new BadRequestError("Please upload an image");
+  }
+
+  const maxSize = 1024 * 1024;
+
+  if (productImage.image.size > maxSize) {
+    throw new BadRequestError(`Image size must be less than ${maxSize}`);
+  }
+
+  const imagePath = new URL(
+    `../public/uploads/${productImage.image.name}`,
+    import.meta.url
+  ).pathname;
+  console.log(imagePath);
+  await productImage.image.mv(imagePath);
+  // Upload an image
+  const uploadResult = await cloudinary.uploader
+    .upload(imagePath, {
+      use_filename: true,
+      folder: "upload-file",
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  console.log(uploadResult);
+
+  // // Optimize delivery by resizing and applying auto-format and auto-quality
+  // const optimizeUrl = cloudinary.url("shoes", {
+  //   fetch_format: "auto",
+  //   quality: "auto",
+  // });
+
+  // console.log(optimizeUrl);
+
+  // // Transform the image: auto-crop to square aspect_ratio
+  // const autoCropUrl = cloudinary.url("shoes", {
+  //   crop: "auto",
+  //   gravity: "auto",
+  //   width: 500,
+  //   height: 500,
+  // });
+
+  // console.log(autoCropUrl);
 };
