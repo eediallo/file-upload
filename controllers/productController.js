68f5c3d9c4227a62eb738da1b967eb73b39b5fef
eediallo <elhhabdouldiallo@gmail.com>
2025-05-09 09:40:19 +0100

    feat: implement createProduct function with validation and error handling

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,24 @@
 import { Product } from "../models/Product.js";
 import { StatusCodes } from "http-status-codes";
+import { BadRequestError } from "../errors/index.js";
 
-export const createProduct = (req, res) => {
-  res.send("Create product");
+export const createProduct = async (req, res) => {
+  try {
+    const { name, price, image } = req.body;
+
+    if (!name || !price || !image) {
+      throw new BadRequestError("All fields are required.");
+    }
+
+    const product = await Product.create({ ...req.body });
+    res.status(StatusCodes.CREATED).json({ success: true, product });
+  } catch (err) {
+    console.error(err.message);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      msg: "Something went wrong, please try again later.",
+    });
+  }
 };
 
 export const getAllProducts = (req, res) => {
